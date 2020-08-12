8c42b4f0e1d89e9fdfd5fd824fb0fb9f41e1bba4
Tullio Bagnoli <tullio.bagnoli@gmail.com>
2020-08-12 17:35:08 +0200

    changes package name because previous already taken on pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="thompson_sampling",
+    name="thompson_sampling_mab",
     version="0.1",
     license='MIT',
     author="Tullio Bagnoli",
