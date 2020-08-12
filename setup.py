189da06ca3c36985dc6965e99d880e0b3d8e9984
Tullio Bagnoli <tullio.bagnoli@gmail.com>
2020-08-12 17:23:10 +0200

    adds license, setup.cfg and makes edits to setup.py for upload to pypi

--- setup.py
+++ setup.py
@@ -5,18 +5,21 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="thompson_sampling",
-    version="0.0.1",
+    version="0.1",
+    license='MIT',
     author="Tullio Bagnoli",
     author_email="tullio.bagnoli@protonmail.com",
     description="Thompson sampling for multi-armed bandit",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/tbagnoli/multi_armed_thompson/src/thompson_sampling/thompson_sampling/",
+    url="https://github.com/tbagnoli/multi_armed_thompson/",
+    download_url='https://github.com/tbagnoli/multi_armed_thompson/archive/v_01.tar.gz',
+    keywords=['thompson', 'sampling', 'unsupervised'],
     packages=setuptools.find_packages(where='src'),
     package_dir={
         '': 'src'
     },
-    install_requires=[],
+    install_requires=['numpy', 'spacy'],
     include_package_data=True,  # forces inclusion of files in MANIFEST.in
     classifiers=[
         "Programming Language :: Python :: 3",
